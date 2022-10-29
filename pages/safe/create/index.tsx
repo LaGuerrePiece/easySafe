305e102d35f0894ad74fc3e27f5087c8088380a5
Stormower <nicolasletimon@gmail.com>
2022-10-29 13:12:37 +0200

    clean

--- pages/safe/create/index.tsx
+++ pages/safe/create/index.tsx
@@ -1,44 +1,7 @@
 import { useRouter } from 'next/router'
 import { Button, ButtonGroup } from '@chakra-ui/react'
 
-import { mailjet } from 'node-mailjet'
-
-
-
-
-
 const Create = () => {
-    mailjet.connect('bad9ff4bc3da4b2dbe719a25edac878c', '39cd9e96d0c3e7a06fb6b682b292a75e')
-    const request = mailjet
-    .post("send", {'version': 'v3.1'})
-    .request({
-      "Messages":[
-        {
-          "From": {
-            "Email": "nicolasletimon@gmail.com",
-            "Name": "Nicolas"
-          },
-          "To": [
-            {
-              "Email": "nicolasletimon@gmail.com",
-              "Name": "Nicolas"
-            }
-          ],
-          "Subject": "Greetings from Mailjet.",
-          "TextPart": "My first Mailjet email",
-          "HTMLPart": "<h3>Dear passenger 1, welcome to <a href='https://www.mailjet.com/'>Mailjet</a>!</h3><br />May the delivery force be with you!",
-          "CustomID": "AppGettingStartedTest"
-        }
-      ]
-    })
-    request
-      .then((result: any) => {
-        console.log(result.body)
-      })
-      .catch((err: any) => {
-        console.log(err.statusCode)
-      })
-
       return <p>
       <Button colorScheme='blue'>Button</Button>
       Safe ID: <br />
